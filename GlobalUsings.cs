f28272ba1e89fe86acfa863abc716a19e711861c
Pody <vladpodyganov@yandex.ru>
2023-02-14 13:30:50 +0300

    Global usings

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,9 +1,13 @@
-﻿
-global using System.Collections.ObjectModel;
+﻿global using System.Collections.ObjectModel;
+global using System.Data;
+global using System.Diagnostics;
+
+global using CommunityToolkit.Mvvm.ComponentModel;
 global using CommunityToolkit.Mvvm.Input;
+
 global using MauiBank.Model;
 global using MauiBank.ViewModel;
 global using MauiBank.View;
-global using System.Data;
 global using MauiBank.Service;
 global using MauiBank.Service.Database;
+global using MauiBank.HTTP;
