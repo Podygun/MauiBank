8495dbe1cc406ad767a0c6a4ec1294347572fd0c
Pody <vladpodyganov@yandex.ru>
2023-03-10 17:59:49 +0300

    laravel

--- Laravel/ServerApp/app/Http/Controllers/api/CardController.php
+++ Laravel/ServerApp/app/Http/Controllers/api/CardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\Card;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CardController extends Controller
 {
@@ -23,7 +24,22 @@ class CardController extends Controller
 
     public function show($id)
     {
-        //
+        return DB::table('cards as c')
+            ->select(
+                'c.number as number'
+                , 'c.cvv as cvv'
+                 , 'c.date_end as date_end'
+                 , 'c.bank_account_id as bank_account_id'
+                 , 'bacc.balance as balance'
+                 , 'types.name as type_name'
+                 , 'valutes.code as valute'
+            )
+            ->join('bank_accounts as bacc','c.bank_account_id','=','bacc.id')
+            ->join('clients as cls','bacc.client_id','=','cls.id')
+            ->join('card_types as types','c.card_type_id','=','types.id')
+            ->join('valutes','bacc.valute_id','=','valutes.id')
+            ->where('cls.user_account_id', $id)
+            ->get();
     }
 
     public function update(Request $request, $id)
--- Laravel/ServerApp/app/Http/Controllers/api/UserDataController.php
+++ Laravel/ServerApp/app/Http/Controllers/api/UserDataController.php
@@ -49,27 +49,14 @@ class UserDataController extends Controller
     {
         $clientId = client::where('user_account_id', $id)->get('id')->first();
 
-        return DB::table('bank_accounts')
-            ->select(
-                'bank_accounts.balance'
-                , 'bank_accounts.client_id'
-                , 'valutes.code as valute_code'
-                , 'clients.first_name'
-                , 'clients.second_name'
-                , 'clients.last_name'
-                , 'clients.phone'
-                , 'clients.email'
-                ,'cards.number'
-                ,'cards.cvv'
-                ,'cards.date_end'
-
-                ,'card_types.name as card_type_name'
+        return DB::table('clients as c')
+            ->select('c.first_name as first_name'
+                 ,'c.second_name as second_name'
+                 , 'c.last_name as last_name'
+                 , 'c.phone as phone'
+                 , 'c.email as email'
             )
-            ->join('valutes','bank_accounts.valute_id','=','valutes.id')
-            ->join('clients','bank_accounts.client_id','=','clients.id')
-            ->join('cards','bank_accounts.id','=','cards.bank_account_id')
-            ->join('card_types','cards.card_type_id','=','card_types.id')
-            ->where('bank_accounts.client_id', $clientId->id)
+            ->where('c.user_account_id', $id)
             ->get();
 
     }
