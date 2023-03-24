94fe32a857f441fd6d7a489bf2a155148428f173
Pody <vladpodyganov@yandex.ru>
2023-03-24 13:19:26 +0300

    Date format for . dates and day of week

--- App/Service/Converters/DateToCultureDate.cs
+++ App/Service/Converters/DateToCultureDate.cs
@@ -7,9 +7,14 @@ public class DateToCultureDate : IValueConverter
 	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
 	{
 		DateOnly sendDate = (DateOnly)value;
-		var theDate = DateOnly.ParseExact(sendDate.ToString(),"M/dd/yyyy" ,CultureInfo.InvariantCulture);
-		
-		return theDate.ToString("m", CultureInfo.GetCultureInfo("ru-RU"));
+		DateOnly theDate = DateOnly.MinValue;
+
+		if(sendDate.ToString().Contains('/'))
+			theDate = DateOnly.ParseExact(sendDate.ToString(),"M/dd/yyyy" ,CultureInfo.InvariantCulture);
+		else if (sendDate.ToString().Contains('.'))
+			theDate = DateOnly.ParseExact(sendDate.ToString(), "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+		return theDate.ToString("ddd, d MMMM", CultureInfo.GetCultureInfo("ru-RU"));
 	}
 
 	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
