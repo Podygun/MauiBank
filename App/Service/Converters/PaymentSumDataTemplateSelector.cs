6a02f2fe85cb1f234c475e0a17e9d242316510b7
Pody <vladpodyganov@yandex.ru>
2023-04-07 18:58:01 +0300

    Colorizing history sum

---
+++ App/Service/Converters/PaymentSumDataTemplateSelector.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiBank.Service.Converters;
+
+class PaymentSumDataTemplateSelector : DataTemplateSelector
+{
+	public DataTemplate NegativeTemplate { get; set; }
+	public DataTemplate PositiveTemplate { get; set; }
+
+	protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+	{
+		return ((HistoryD)item).Sum <= 0 ? NegativeTemplate : PositiveTemplate;
+	}
+}
---
+++ App/Service/Converters/SumSignConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace MauiBank.Service.Converters;
+
+    class SumSignConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		string str = value.ToString();
+		if ((double)value >= 0)
+		{
+			return "+" + str;
+		}
+
+		return "-" + str;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
