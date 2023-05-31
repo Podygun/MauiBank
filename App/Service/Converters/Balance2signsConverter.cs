b3dec93003fa6d7770851b470a0bde3d04bd9a69
Pody <vladpodyganov@yandex.ru>
2023-05-31 17:54:44 +0300

    round double value to 2 sign

---
+++ App/Service/Converters/Balance2signsConverter.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Globalization;
+
+namespace MauiBank.Service.Converters;
+
+public class Balance2signsConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		double oldValue = (double)value;
+		return Math.Round(oldValue, 2);
+		
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		return String.Empty;
+	}
+}
